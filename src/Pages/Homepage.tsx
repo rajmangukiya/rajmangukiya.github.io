db7f1c34256e43a0cabb466f935640d672896157
rajmangukiya <rajmangukiya00@gmail.com>
2021-08-30 15:53:22 +0530

    svg animation done

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Button, Container } from 'react-bootstrap'
 import { useHistory, useLocation } from 'react-router-dom';
+import Background from '../Components/Background';
 
 interface homepageProps {
 
@@ -11,8 +12,9 @@ const Homepage: React.FC<homepageProps> = () => {
     return (
         <div className="homepage">
             <Container>
-                <p>Home Page</p>
+                
             </Container>
+            <Background />
         </div>
     )
 }
